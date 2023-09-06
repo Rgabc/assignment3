ff176b4121fba17d361a204b527f8ee66674c8e1
Rgabc <rohitgajbhiye4@gmail.com>
2023-09-06 22:47:02 +0530

    my first

---
+++ script.js
@@ -0,0 +1,69 @@
+// 1st question
+// let oddOrEven=parseInt(prompt("lets check its odd or even input any number"))
+
+// if(oddOrEven%2===0){
+//     alert("the Number is Even =>"+oddOrEven)
+      
+// }else if(oddOrEven%2===1){
+//     alert("the Number is odd=>"+oddOrEven)
+   
+    
+//         }
+
+
+// else{
+//     alert("enter the number")
+    
+// }
+ 
+
+//2 question
+// let firstNumber=parseInt(prompt("enter 1st number"));
+// let secondNumber=parseInt(prompt("enter 2nd number"));
+// if(firstNumber>secondNumber){
+//     alert("The first number is greater than the second number")
+// }
+
+// else if(firstNumber<secondNumber)
+// {
+//     alert("The second number is greater then the first")
+  
+    
+//         }
+
+// else{
+//     alert("enter the number")
+// }
+//3 question
+// let randomNumber=parseInt(prompt("enter the random number"));
+// if(randomNumber<10){
+//     alert("The Number is less than 10")
+// }else if(randomNumber>=10){
+//     alert("the number is greater than or equal to 10")
+
+// }else{  alert("enter the number")}
+// 4th question 
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+//5th question
+let q=1;
+do {
+    if(q%2===0){
+        console.log("even",q)
+    }
+    q++;
+ 
+    
+} while (q<=10){
+    console.log("end it")
+}
+//6 question
+for(let i=10;i>=1;i--){
+    if(i%2===1){
+        console.log("odd",i)
+
+    }
+}
