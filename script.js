660a39047758c91417889f9b4dc1299ec6319f7a
Rgabc <rohitgajbhiye4@gmail.com>
2023-09-06 22:49:57 +0530

    my first

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 // 1st question
-// let oddOrEven=parseInt(prompt("lets check its odd or even input any number"))
+let oddOrEven=parseInt(prompt("lets check its odd or even input any number"))
 
-// if(oddOrEven%2===0){
-//     alert("the Number is Even =>"+oddOrEven)
+if(oddOrEven%2===0){
+    alert("the Number is Even =>"+oddOrEven)
       
-// }else if(oddOrEven%2===1){
-//     alert("the Number is odd=>"+oddOrEven)
+}else if(oddOrEven%2===1){
+    alert("the Number is odd=>"+oddOrEven)
    
     
-//         }
+        }
 
 
-// else{
-//     alert("enter the number")
+else{
+    alert("enter the number")
     
-// }
+}
  
 
-//2 question
-// let firstNumber=parseInt(prompt("enter 1st number"));
-// let secondNumber=parseInt(prompt("enter 2nd number"));
-// if(firstNumber>secondNumber){
-//     alert("The first number is greater than the second number")
-// }
+// 2 question
+let firstNumber=parseInt(prompt("enter 1st number"));
+let secondNumber=parseInt(prompt("enter 2nd number"));
+if(firstNumber>secondNumber){
+    alert("The first number is greater than the second number")
+}
 
-// else if(firstNumber<secondNumber)
-// {
-//     alert("The second number is greater then the first")
+else if(firstNumber<secondNumber)
+{
+    alert("The second number is greater then the first")
   
     
-//         }
+        }
 
-// else{
-//     alert("enter the number")
-// }
-//3 question
-// let randomNumber=parseInt(prompt("enter the random number"));
-// if(randomNumber<10){
-//     alert("The Number is less than 10")
-// }else if(randomNumber>=10){
-//     alert("the number is greater than or equal to 10")
+else{
+    alert("enter the number")
+}
+// 3 question
+let randomNumber=parseInt(prompt("enter the random number"));
+if(randomNumber<10){
+    alert("The Number is less than 10")
+}else if(randomNumber>=10){
+    alert("the number is greater than or equal to 10")
 
-// }else{  alert("enter the number")}
+}else{  alert("enter the number")}
 // 4th question 
 let i=1;
 while(i<=10){
